48c4208ee5a420308c8114e62abf06450cd7b890
Teo_TM <matteomas06@gmail.com>
2024-12-12 00:10:08 +0100

    Finished

--- C/Verifica.c
+++ C/Verifica.c
@@ -2,25 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-//#define ASSETS_PATH "C:\\Users\\matte\\Documents\\VSCode\\Assets\\"
-#define ASSETS_PATH "C:\\Users\\mat.maspero\\VSCode\\Assets\\"
-#define INPUT_FILE "listacomuni"
+#define ABSPATH "C:/users/matte/documents/vscode/C/"	//	Se necessario cambiare il path, altrimenti lasciare vuoto: ""
+#define LISTACOMUNI "listacomuni"
+#define ISTATLENGTH 5
+#define NOMELENGTH 32
+#define PROVINCIALENGTH 3
+#define REGIONELENGTH 32
+#define PREFISSOLENGTH 4
+#define CODICEPOSTALELENGTH 6
+#define CODICEFISCALELENGTH 8
+#define LINKLENGTH 32
 
 /*
 	TYPES
 */
 
 typedef struct Comune {
-	char *istat;
-	char *name;
-	char *province;
-	char *region;
-	char *prefix;
-	char *postalCode;
-	char *fiscalCode;
-	int population;
-	char *link;
-	struct Comune* next;
+	char istat[ISTATLENGTH];
+	char nome[NOMELENGTH];
+	char provincia[PROVINCIALENGTH];
+	char prefisso[PREFISSOLENGTH];
+	char codicePostale[CODICEPOSTALELENGTH];
+	char codiceFiscale[CODICEFISCALELENGTH];
+	int popolazione;
+	char regione[REGIONELENGTH];
+	char link[LINKLENGTH];
+	struct Comune *next;
 } Comune;
 
 typedef Comune* Node;
@@ -29,47 +36,70 @@ typedef Comune* Node;
 	PROTOYPES
 */
 
+void printComuniPerProvince(Node headRegione);
+int countComuniOfProvince(Node headRegione, char *provincia);
+void printInhabitantsPerProvince(Node headRegione);
+char *getProvinceHash(Node headRegione, int *length);
+int countInhabitantsOfProvince(Node headRegione, char *provincia);
+Node createListFromRegionFile(Node headRegione, char *filename);
+void saveRegionDataToFile(Node headRegione, char *filename);
+void clearConsoleAndPrintMenu();
+Node createListFromTxtFile(Node head, const char *filename);
+
+char *inputString(const char *prompt, int maxLength);
 int inputInt(const char *prompt, int min, int max);
-char *inputStr(const char *prompt);
+const char *buildFilename(char* name, const char *extension);
 
-Node createNode(char *istat, char *name, char *province, char *region, char *prefix, char *postalCode, char *fiscalCode, int population, char *link);
-Node appendNode(Node head, char *istat, char *name, char *province, char *region, char *prefix, char *postalCode, char *fiscalCode, int population, char *link);
-Node freeList(Node head);
-
-void printMenu();
-
-const char *getFullPath(char *fileName, const char *extension);
-Node loadComuni(Node head, char *fileName);
-void saveDataToDatFile(Node head, char *fileName);
-void readDataFromDatFile(char *fileName);
+Node createNode(char *istat, char *nome, char *provincia, char *regione, char *prefisso, char *codicePostale, char *codiceFiscale, int popolazione, char *link);
+Node pushNode(Node head, char *istat, char *nome, char *provincia, char *regione, char *prefisso, char *codicePostale, char *codiceFiscale, int popolazione, char *link);
+void destroyList(Node head);
+void printList(Node head);
 
 /*
 	MAIN
 */
 
 int main() {
-	Node head = NULL;
-	int choice;
-	char *fileName;
-	head = loadComuni(head, (char*) INPUT_FILE);
+	Node headComuni = NULL;
+	headComuni = createListFromTxtFile(headComuni, LISTACOMUNI);
 
 	while(1) {
-		printMenu();
-		choice = inputInt("Scelta: ", 0, 4);
-
-		switch(choice) {
-		case 1:
-			fileName = inputStr("Inserisci il nome della regione: ");
-			saveDataToDatFile(head, fileName);
-			break;
-		case 2:
-			fileName = inputStr("Inserisci il nome della regione: ");
-			readDataFromDatFile(fileName);
-			break;
-		case 0:
+		clearConsoleAndPrintMenu();
+		int choice = inputInt("Scelta: ", 0, 4);
+		
+		if(choice == 0) {
 			printf("Arrivederci!\n");
 			return 0;
 		}
+
+		char *filename;
+		Node headRegione = NULL;
+
+		switch(choice) {
+		case 1:
+			filename = inputString("Scrivi il nome della regione di cui si vogliono salvare i dati: ", REGIONELENGTH);
+			saveRegionDataToFile(headComuni, filename);
+			break;
+		case 2:
+			filename = inputString("Scrivi il nome della regione di cui si vogliono visualizzare i dati salvati: ", REGIONELENGTH);
+			headRegione = createListFromRegionFile(headRegione, filename);
+			printf("Regione: %s\n", filename);
+			printList(headRegione);
+			break;
+		case 3:
+			filename = inputString("Scrivi il nome della regione di cui si vogliono visualizzare il totale di abitanti per provincia: ", REGIONELENGTH);
+			headRegione = createListFromRegionFile(headRegione, filename);
+			printInhabitantsPerProvince(headRegione);
+			break;
+		case 4:
+			filename = inputString("Scrivi il nome della regione di cui si vogliono visualizzare il totale di comuni per provincia: ", REGIONELENGTH);
+			headRegione = createListFromRegionFile(headRegione, filename);
+			printComuniPerProvince(headRegione);
+			break;
+		}
+
+		printf("Premi invio per continuare...");
+		getchar();
 	}
 
 	return 0;
@@ -79,186 +109,269 @@ int main() {
 	FUNCTIONS
 */
 
-int inputInt(const char *prompt, int min, int max) {
-	int input;
-	printf(prompt);
-	scanf("%d", &input);
+void printComuniPerProvince(Node headRegione) {
+	int lenght = 0;
+	char *provinceHash = getProvinceHash(headRegione, &lenght);
+	int i;
 
-	if(min > max)
-		return input;
-
-	while(input < min || input > max) {
-		printf("Valore non valido. Riprova: ");
-		scanf("%d", &input);
+	for(i = 0; i < lenght; i = i + 3) {
+		int total = countComuniOfProvince(headRegione, provinceHash + i);
+		printf("Totale di comuni nella provincia '%s': %d\n", provinceHash + i, total);
 	}
-
-	return input;
 }
 
-char *inputStr(const char *prompt) {
-	char temp[32];
-	printf(prompt);
-	while(getchar() != '\n');	
-	scanf("%[^\n]", temp);
+int countComuniOfProvince(Node headRegione, char *provincia) {
+	int total = 0;
 
-	char *result = (char*) malloc(sizeof(char) * (strlen(temp) + 1));
-	strcpy(result, temp);
+	while(headRegione != NULL) {
+		if(strcmp(headRegione->provincia, provincia) == 0)
+			total ++;
+		
+		headRegione = headRegione->next;
+	}
+
+	return total;
+}
+
+void printInhabitantsPerProvince(Node headRegione) {
+	int lenght = 0;
+	char *provinceHash = getProvinceHash(headRegione, &lenght);
+	int i;
+
+	for(i = 0; i < lenght; i = i + 3) {
+		int total = countInhabitantsOfProvince(headRegione, provinceHash + i);
+		printf("Totale di abitanti nella provincia '%s': %d\n", provinceHash + i, total);
+	}
+}
+
+char *getProvinceHash(Node headRegione, int *length) {
+	char *result = (char*) malloc(sizeof(char));
+	int flag;
+	int i;
+	*length = 0;
+
+	while(headRegione != NULL) {
+		flag = 1;
+
+		for(i = 0; i < *length; i = i + 3)
+			if(strcmp(result + i, headRegione->provincia) == 0)
+				flag = 0;
+
+		if(flag) {
+			result = (char*) realloc(result, strlen(result) + sizeof(char) * 3);	//	length + \0 + strlen(provincia) + \0
+			memcpy(result + *length, headRegione->provincia, 3);
+			*length = *length + 3;
+		}
+
+		headRegione = headRegione->next;
+	}
+
 	return result;
 }
 
-Node createNode(char *istat, char *name, char *province, char *region, char *prefix, char *postalCode, char *fiscalCode, int population, char *link) {
-	Node newNode = (Node) malloc(sizeof(Comune));
-	newNode->istat = istat;
-	newNode->name = name;
-	newNode->province = province;
-	newNode->region = region;
-	newNode->prefix = prefix;
-	newNode->postalCode = postalCode;
-	newNode->fiscalCode = fiscalCode;
-	newNode->population = population;
-	newNode->link = link;
-	newNode->next = NULL;
-	return newNode;
-}
+int countInhabitantsOfProvince(Node headRegione, char *provincia) {
+	int total = 0;
 
-Node appendNode(Node head, char *istat, char *name, char *province, char *region, char *prefix, char *postalCode, char *fiscalCode, int population, char *link) {
-	if(head == NULL)
-		return createNode(istat, name, province, region, prefix, postalCode, fiscalCode, population, link);
-	
-	Node current = head;
-	
-	while(current->next != NULL)
-		current = current->next;
-	
-	current->next = createNode(istat, name, province, region, prefix, postalCode, fiscalCode, population, link);;
-	return head;
-}
-
-Node freeList(Node head) {
-	Node toBeFreed;
-
-	while(head != NULL) {
-		toBeFreed = head;
-		free(toBeFreed);
-		head = head->next;
+	while(headRegione != NULL) {
+		if(strcmp(headRegione->provincia, provincia) == 0)
+			total = total + headRegione->popolazione;
+		
+		headRegione = headRegione->next;
 	}
 
-	return NULL;
+	return total;
 }
 
-void printMenu() {
+Node createListFromRegionFile(Node headRegione, char *filename) {
+	FILE *file = fopen(buildFilename(filename, ".dat"), "rb");
+
+	if(file == NULL) {
+		printf("Errore nell'apertura del file '%s'\n", filename);
+		exit(1);
+	}
+
+	if(headRegione != NULL) {
+		printf("La lista data non è vuota, i suoi dati verranno cancellati.\n");
+		destroyList(headRegione);
+	}
+
+	char istat[ISTATLENGTH];
+	char nome[NOMELENGTH];
+	char provincia[PROVINCIALENGTH];
+	char prefisso[PREFISSOLENGTH];
+	char codicePostale[CODICEPOSTALELENGTH];
+	char codiceFiscale[CODICEFISCALELENGTH];
+	int popolazione;
+
+	while(fread(istat, sizeof(char), ISTATLENGTH, file) == ISTATLENGTH) {
+		long nameBegin = ftell(file);
+		fread(nome, sizeof(char), NOMELENGTH, file);
+		int nameLength = strlen(nome) + 1;
+		fseek(file, nameBegin + nameLength, SEEK_SET);
+
+		fread(provincia, sizeof(char), PROVINCIALENGTH, file);
+		fread(prefisso, sizeof(char), PREFISSOLENGTH, file);
+		fread(codicePostale, sizeof(char), CODICEPOSTALELENGTH, file);
+		fread(codiceFiscale, sizeof(char), CODICEFISCALELENGTH, file);
+		fread(&popolazione, sizeof(int), 1, file);
+		headRegione = pushNode(headRegione, istat, nome, provincia, filename, prefisso, codicePostale, codiceFiscale, popolazione, (char*) "");
+	}
+
+	fclose(file);
+	while(getchar() != '\n');
+	return headRegione;
+}
+
+void saveRegionDataToFile(Node headRegione, char *filename) {
+	FILE *file = fopen(buildFilename(filename, ".dat"), "wb");
+
+	if(file == NULL) {
+		printf("Errore nell'apertura del file '%s'\n", filename);
+		exit(1);
+	}
+
+	while(headRegione != NULL) {
+		if(strcmp(headRegione->regione, filename) != 0) {
+			headRegione = headRegione->next;
+			continue;
+		}
+
+		fwrite(headRegione->istat, sizeof(char), ISTATLENGTH, file);
+		fwrite(headRegione->nome, sizeof(char), strlen(headRegione->nome) + 1, file);
+		fwrite(headRegione->provincia, sizeof(char), PROVINCIALENGTH, file);
+		fwrite(headRegione->prefisso, sizeof(char), PREFISSOLENGTH, file);
+		fwrite(headRegione->codicePostale, sizeof(char), CODICEPOSTALELENGTH, file);
+		fwrite(headRegione->codiceFiscale, sizeof(char), CODICEFISCALELENGTH, file);
+		fwrite(&headRegione->popolazione, sizeof(int), 1, file);
+		headRegione = headRegione->next;
+	}
+
+	fclose(file);
+}
+
+void clearConsoleAndPrintMenu() {
+	//	system("cls");	Buggato su VSCode
+	printf("\e[1;1H\e[2J");	//	Equivalente di system("cls")
+
 	printf(
-		"+-------------------------------------------------------------------------------+\n"
-		"| 1. Salva i dati di una regione a scelta su file                               |\n"
-		"| 2. Visualizza i dati di una regione a scelta                                  |\n"
-		"| 3. Visualizza il numero di abitanti di ogni provincia in una regione a scelta |\n"
-		"| 4. Visualizza il numero di comuni in una regione a scelta                     |\n"
-		"| 0. Termina il loop                                                            |\n"
-		"+-------------------------------------------------------------------------------+\n"
+		"Operazioni disponibili:\n"
+		"1. Scegli una regione e salvane i dati su file;\n"
+		"2. Scegli una regione e visualizzane i dati salvati;\n"
+		"3. Scegli una regione e visualizzane il totale di abitanti per provincia;\n"
+		"4. Scegli una regione e visualizzane il totale di comuni per provincia;\n"
+		"0. Esci.\n"
 	);
 }
 
-const char *getFullPath(char *fileName, const char *extension) {
-	char *fullPath = (char*) malloc( sizeof(char) * (strlen(ASSETS_PATH) + strlen(fileName) + strlen(extension) + 1) );
-	strcpy(fullPath, ASSETS_PATH);
-	strcat(fullPath, fileName);
-	strcat(fullPath, extension);
-	return (const char*) fullPath;
-}
+Node createListFromTxtFile(Node head, const char *filename) {
+	FILE *file = fopen(buildFilename( (char*) LISTACOMUNI, ".txt" ), "r");
 
-Node loadComuni(Node head, char *fileName) {
-	FILE *inputFile = fopen(getFullPath(fileName, ".txt"), "r");
-
-	if(inputFile == NULL) {
-		printf("Errore nell'apertura del file %s\n", fileName);
+	if(file == NULL) {
+		printf("Errore nell'apertura del file '%s'\n", filename);
 		exit(1);
 	}
 
-	if(head != NULL)
-		head = freeList(head);
-
-	int population;
-
-	while(1) {
-		char *istat = (char*) malloc(sizeof(char) * 5);
-		char *name = (char*) malloc(sizeof(char) * 32);
-		char *province = (char*) malloc(sizeof(char) * 3);
-		char *region = (char*) malloc(sizeof(char) * 32);
-		char *prefix = (char*) malloc(sizeof(char) * 4);
-		char *postalCode = (char*) malloc(sizeof(char) * 6);
-		char *fiscalCode = (char*) malloc(sizeof(char) * 8);
-		char *link = (char*) malloc(sizeof(char) * 64);
-		
-		if(fscanf(inputFile, "%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%d;%s\n", istat, name, province, region, prefix, postalCode, fiscalCode, &population, link) != 9)
-			break;
-
-		head = appendNode(head, istat, name, province, region, prefix, postalCode, fiscalCode, population, link);
+	if(head != NULL) {
+		printf("La lista data non è vuota, i suoi dati verranno cancellati.\n");
+		destroyList(head);
 	}
 
-	fclose(inputFile);
+	head = NULL;
+	char istat[ISTATLENGTH];
+	char nome[NOMELENGTH];
+	char provincia[PROVINCIALENGTH];
+	char regione[REGIONELENGTH];
+	char prefisso[PREFISSOLENGTH];
+	char codicePostale[CODICEPOSTALELENGTH];
+	char codiceFiscale[CODICEFISCALELENGTH];
+	int popolazione;
+	char link[LINKLENGTH];
+
+	while(fscanf(file, "%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%d;%s\n", istat, nome, provincia, regione, prefisso, codicePostale, codiceFiscale, &popolazione, link) == 9)
+		head = pushNode(head, istat, nome, provincia, regione, prefisso, codicePostale, codiceFiscale, popolazione, link);
+	
+	fclose(file);
 	return head;
 }
 
-void saveDataToDatFile(Node head, char *fileName) {
-	FILE *outputFile = fopen(getFullPath(fileName, ".dat"), "wb");
-
-	if(outputFile == NULL) {
-		printf("Errore nell'apertura del file %s\n", fileName);
-		exit(1);
-	}
-
-	while(head != NULL) {
-		if(strcmp(head->region, fileName) == 0) {
-			fwrite(head->istat, sizeof(char), strlen(head->istat) + 1, outputFile);
-			fwrite(head->name, sizeof(char), strlen(head->name) + 1, outputFile);
-			fwrite(head->province, sizeof(char), strlen(head->province) + 1, outputFile);
-			fwrite(head->prefix, sizeof(char), strlen(head->prefix) + 1, outputFile);
-			fwrite(head->postalCode, sizeof(char), strlen(head->postalCode) + 1, outputFile);
-			fwrite(head->fiscalCode, sizeof(char), strlen(head->fiscalCode) + 1, outputFile);
-			fwrite(&(head->population), sizeof(int), 1, outputFile);
-			fwrite("\n", sizeof(char), 2, outputFile);
-		}
-
-		head = head->next;
-	}
-
-	fclose(outputFile);
+char *inputString(const char *prompt, int maxLength) {
+	printf(prompt);
+	char *result = (char*) malloc(maxLength);
+	scanf("%s", result);
+	return (char*) realloc(result, strlen(result) + 1);
 }
 
-void readDataFromDatFile(char *fileName) {
-	FILE *inputFile = fopen(getFullPath(fileName, ".dat"), "rb");
+int inputInt(const char *prompt, int min, int max) {
+	printf(prompt);
+	int result;
+	scanf("%d", &result);
 
-	if(inputFile == NULL) {
-		printf("Errore nell'apertura del file %s\n", fileName);
-		exit(1);
+	if(min > max)
+		return result;
+
+	while(result < min || result > max) {
+		printf("Input non valido. Riprova: ");
+		scanf("%d", &result);
 	}
 
-	char istat[5];
-	char name[32];
-	char province[3];
-	char prefix[4];
-	char postalCode[6];
-	char fiscalCode[8];
-	int population;
+	return result;
+}
 
-	while(fread(istat, sizeof(char), sizeof(istat), inputFile) == sizeof(istat)) {
-		fread(name, sizeof(char), sizeof(name), inputFile);
-		fread(province, sizeof(char), sizeof(province), inputFile);
-		fread(prefix, sizeof(char), sizeof(prefix), inputFile);
-		fread(postalCode, sizeof(char), sizeof(postalCode), inputFile);
-		fread(fiscalCode, sizeof(char), sizeof(fiscalCode), inputFile);
-		fread(&population, sizeof(int), 1, inputFile);
-		fseek(inputFile, 1, SEEK_CUR);
+const char *buildFilename(char* name, const char *extension) {
+	char *result = (char*) malloc(strlen(name) + strlen(extension) + 1);
+	strcpy(result, ABSPATH);
+	strcat(result, name);
+	strcat(result, extension);
+	return (const char*) result;
+}
 
-		printf("\tIstat: %s\n", istat);
-		printf("\tName: %s\n", name);
-		printf("\tProvince: %s\n", province);
-		printf("\tPrefix: %s\n", prefix);
-		printf("\tPostal Code: %s\n", postalCode);
-		printf("\tFiscal Code: %s\n", fiscalCode);
-		printf("\tPopulation: %d\n", population);
-		printf("\n");
+Node createNode(char *istat, char *nome, char *provincia, char *regione, char *prefisso, char *codicePostale, char *codiceFiscale, int popolazione, char *link) {
+	Node result = (Node) malloc(sizeof(Comune));
+	strcpy(result->istat, istat);
+	strcpy(result->nome, nome);
+	strcpy(result->provincia, provincia);
+	strcpy(result->prefisso, prefisso);
+	strcpy(result->codicePostale, codicePostale);
+	strcpy(result->codiceFiscale, codiceFiscale);
+	result->popolazione = popolazione;
+	strcpy(result->regione, regione);
+	strcpy(result->link, link);
+	result->next = NULL;
+	return result;
+}
+
+Node pushNode(Node head, char *istat, char *nome, char *provincia, char *regione, char *prefisso, char *codicePostale, char *codiceFiscale, int popolazione, char *link) {
+	Node result = createNode(istat, nome, provincia, regione, prefisso, codicePostale, codiceFiscale, popolazione, link);
+
+	if(head == NULL)
+		return result;
+	
+	result->next = head;
+	return result;
+}
+
+void destroyList(Node head) {
+	Node next;
+	
+	while(head != NULL) {
+		next = head->next;
+		free(head);
+		head = next;
 	}
+}
 
-	fclose(inputFile);
+void printList(Node head) {
+	while(head != NULL) {
+		printf(
+			"Istat: %s\n"
+			"Nome: %s\n"
+			"Provincia: %s\n"
+			"Prefisso: %s\n"
+			"Codice postale: %s\n"
+			"Codice fiscale: %s\n"
+			"Popolazione: %d\n\n",
+			head->istat, head->nome, head->provincia, head->regione, head->prefisso, head->codicePostale, head->codiceFiscale, head->popolazione, head->link
+		);
+		head = head->next;
+	}
 }
