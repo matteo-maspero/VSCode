b578416a27284b7fa9cdd5a4bc967d0e4763adc1
Teo_TM <matteomas06@gmail.com>
2025-01-20 19:11:43 +0100

    Cleanup & New hierarchy

---
+++ VLSM/VLSM.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//	@brief Sequence of 8 bits.
+typedef unsigned char Byte;
+
+//	@brief Sequence of 32 bits.
+typedef unsigned int Word, uint;
+
+int main() {
+	return 0;
+}
