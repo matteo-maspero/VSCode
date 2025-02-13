64725550238fea277b9afda8899de6ab967418a0
Teo_TM <matteomas06@gmail.com>
2025-02-14 00:44:23 +0100

    commit

--- Scheduler/GUI.py
+++ Scheduler/GUI.py
@@ -1,157 +1,138 @@
 from tkinter import *
-from Job import Job
-import matplotlib.pyplot as plt
+from dateutil.utils import within_delta
+from matplotlib import pyplot
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from Job import Job, State, JobInfo
+from main import Scheduler
 
 """
-	Color palette
+	COLOR PALETTE
 """
-GREY1 = "#1a1a1a"
-GREY2 = "#2a2a2a"
-GREY3 = "#3a3a3a"
-WHITE1 = "#e5e5e5"
+GREY1 = "#1e1f22"
+GREY2 = "#2b2d30"
+GREY3 = "#3b3d40"
+GREY4 = "#4b4d50"
+RED1 = "#cc0000"
+RED2 = "#aa1111"
+WHITE1 = "#f0f0f0"
 
-class Gui:
-	def __init__(self, scheduler):
-		self.scheduler = scheduler
+class GUI:
+	def __init__(self, scheduler: Scheduler):
+		self.scheduler: Scheduler = scheduler
+		self.x: int = 0
+		self.y: int = 0
 
-		self.setupRoot()
-		self.setupWindow()
-		self.setupJobsList()
-		self.setupJobInput()
-		self.setupGanttChart()
-		self.setupStatistics()
-		self.setupResetButton()
-
-	def setupRoot(self):
+		"""
+			ROOT CONFIGURATION
+		"""
 		self.root = Tk()
-		self.root.title("Scheduler GUI")
-		self.root.iconbitmap('icon.ico')
-		self.root.geometry("1000x600")
+		self.root.geometry(f"{self.get_x_perc(70)}x{self.get_y_perc(70)}")
 		self.root.state("zoomed")
+		self.root.overrideredirect(True)
 
-	def setupWindow(self):
-		self.window = Frame(master=self.root)
+		"""
+			WINDOW CONFIGURATION
+		"""
+		self.window = Frame(master=self.root, bg=GREY1)
 		self.window.pack(fill=BOTH, expand=True)
-		self.window.configure(bg = GREY1)
-		self.window.grid_columnconfigure(0, weight=1)
-		self.window.grid_columnconfigure(1, weight=4)
-		self.window.grid_rowconfigure(0, weight=1)
-		self.window.grid_rowconfigure(1, weight=20)
-		self.window.grid_rowconfigure(2, weight=10)
-		self.window.grid_rowconfigure(3, weight=1)
+		self.window.grid_columnconfigure(index=0, weight=1)
+		self.window.grid_columnconfigure(index=1, weight=3)
+		self.window.grid_rowconfigure(index=0, minsize=self.get_y_perc(4))
+		self.window.grid_rowconfigure(index=1, weight=4)
+		self.window.grid_rowconfigure(index=2, weight=1)
+		self.window.grid_rowconfigure(index=3, weight=2)
 
-	def setupJobsList(self):
-		self.jobsList = Listbox(self.window, bg=GREY1, fg=WHITE1, bd=0, highlightthickness=1, highlightbackground=GREY2, highlightcolor=GREY3)
-		self.jobsList.grid(row=0, column=0, rowspan=2, sticky="nsew")
+		"""
+			HEADER CONFIGURATION
+		"""
+		self.header = Frame(master=self.window, bg=GREY2, highlightthickness=.5, highlightbackground=GREY1, highlightcolor=GREY1, border=0)
+		self.header.grid(row=0, column=0, columnspan=2, sticky="nsew")
 
-	def setupJobInput(self):
-		self.jobInputFrame = Frame(self.window, bg=GREY1, highlightthickness=1, highlightbackground=GREY2, highlightcolor=GREY3)
-		self.jobInputFrame.grid(row=2, column=0, sticky="nsew")
+		# Make window draggable
+		self.header.bind("<ButtonPress-1>", self.start_move)
+		self.header.bind("<ButtonRelease-1>", self.stop_move)
+		self.header.bind("<B1-Motion>", self.do_move)
 
-		Label(self.jobInputFrame, text="Add a new process:", bg=GREY1, fg=WHITE1, font=("Helvetica", 16)).grid(row=0, column=0, columnspan=2, pady=10)
+		# Icon
+		self.icon = PhotoImage(file="icon.png").subsample(2)
+		self.icon_label = Label(master=self.header, image=self.icon, bg=GREY2)
+		self.icon_label.pack(side=LEFT, fill=Y)
 
-		Label(self.jobInputFrame, text="Job ID", bg=GREY1, fg=WHITE1).grid(row=1, column=0, padx=5, pady=5)
-		self.jobIdEntry = Entry(self.jobInputFrame, bg=GREY2, fg=WHITE1)
-		self.jobIdEntry.grid(row=1, column=1, padx=5, pady=5)
+		# Title
+		self.title = Label(master=self.header, text="aaa GUI by Maspero, Zappa & Sorbara", bg=GREY2, fg=WHITE1, font=("Arial", 10), border=0)
+		self.title.pack(side=LEFT, fill=Y)
 
-		Label(self.jobInputFrame, text="Priority", bg=GREY1, fg=WHITE1).grid(row=2, column=0, padx=5, pady=5)
-		self.priorityEntry = Entry(self.jobInputFrame, bg=GREY2, fg=WHITE1)
-		self.priorityEntry.grid(row=2, column=1, padx=5, pady=5)
+		# Close Button
+		self.close_button = Button(master=self.header, text="✕", justify="center", bg=GREY2, fg=WHITE1, font=("Arial", 10), command=self.root.destroy, relief=FLAT, highlightthickness=0, activebackground=RED2, activeforeground=WHITE1, border=0)
+		self.close_button.pack(side=RIGHT, fill=Y, ipadx=10)
+		self.close_button.bind("<Enter>", lambda _: self.close_button.config(bg=RED1))
+		self.close_button.bind("<Leave>", lambda _: self.close_button.config(bg=GREY2))
 
-		Label(self.jobInputFrame, text="Arrival Time", bg=GREY1, fg=WHITE1).grid(row=3, column=0, padx=5, pady=5)
-		self.arrivalTimeEntry = Entry(self.jobInputFrame, bg=GREY2, fg=WHITE1)
-		self.arrivalTimeEntry.grid(row=3, column=1, padx=5, pady=5)
+		# Window Button
+		self.window_button = Button(master=self.header, text="🗗", justify="center", bg=GREY2, fg=WHITE1, font=("Arial", 16), command=self.toggle_window, relief=FLAT, highlightthickness=0, activebackground=GREY4, activeforeground=WHITE1, border=0)
+		self.window_button.pack(side=RIGHT, fill=Y, ipadx=10)
+		self.window_button.bind("<Enter>", lambda _: self.window_button.config(bg=GREY3))
+		self.window_button.bind("<Leave>", lambda _: self.window_button.config(bg=GREY2))
 
-		Label(self.jobInputFrame, text="Burst Time", bg=GREY1, fg=WHITE1).grid(row=4, column=0, padx=5, pady=5)
-		self.burstTimeEntry = Entry(self.jobInputFrame, bg=GREY2, fg=WHITE1)
-		self.burstTimeEntry.grid(row=4, column=1, padx=5, pady=5)
+		"""
+			PROCESSES LIST CONFIGURATION
+		"""
+		self.list = Frame(master=self.window, bg=GREY2, highlightthickness=.5, highlightbackground=GREY1, highlightcolor=GREY1, border=0, width=0)
+		self.list.grid(row=1, column=0, sticky="nsew")
+		self.update_list([])
 
-		self.addButton = Button(self.jobInputFrame, text="Add", command=self.addJob, bg=GREY3, fg=WHITE1)
-		self.addButton.grid(row=5, column=0, pady=10)
+		"""
+			GANTT CHART CONFIGURATION
+		"""
+		self.chart = Frame(master=self.window, bg=GREY1, highlightthickness=.5, highlightbackground=GREY2, highlightcolor=GREY2, border=0)
+		self.chart.grid(row=1, rowspan=2, column=1, sticky="nsew")
 
-		self.clearButton = Button(self.jobInputFrame, text="Clear", command=self.clearInputs, bg=GREY3, fg=WHITE1)
-		self.clearButton.grid(row=5, column=1, pady=10)
+		"""
+			INPUTS CONFIGURATION
+		"""
+		self.inputs = Frame(master=self.window, bg=GREY2, highlightthickness=.5, highlightbackground=GREY1, highlightcolor=GREY1, border=0)
+		self.inputs.grid(row=2, rowspan=2, column=0, sticky="nsew")
 
-		self.fcfsButton = Button(self.jobInputFrame, text="Execute FCFS", command=self.scheduler.executeFCFS, bg=GREY3, fg=WHITE1)
-		self.fcfsButton.grid(row=6, column=0, pady=10)
-
-		self.sjfButton = Button(self.jobInputFrame, text="Execute SJF", command=self.scheduler.executeSJF, bg=GREY3, fg=WHITE1)
-		self.sjfButton.grid(row=6, column=1, pady=10)
-
-		#self.rrButton = Button(self.jobInputFrame, text="Execute RR", command=self.scheduler.run, bg=GREY3, fg=WHITE1)
-		#self.rrButton.grid(row=8, column=0, pady=10, columnspan=2)
-
-	def addJob(self):
-		try:
-			name = self.jobIdEntry.get()
-			if name == "":
-				raise ValueError
-			priority = int(self.priorityEntry.get())
-			arrival = int(self.arrivalTimeEntry.get())
-			burst = int(self.burstTimeEntry.get())
-		except:
-			self.clearInputs()
-			return
-		
-		self.scheduler.addJob(Job(name, priority, arrival, burst))
-
-	def clearInputs(self):
-		self.jobIdEntry.delete(0, END)
-		self.priorityEntry.delete(0, END)
-		self.arrivalTimeEntry.delete(0, END)
-		self.burstTimeEntry.delete(0, END)
-		
-	def updateJobsList(self):
-		self.jobsList.delete(0, END)
-		for job in self.scheduler.readyQueue:
-			self.jobsList.insert(END, f"Job ID: {job.name}, Priority: {job.priority}, Arrival: {job.arrival}, Burst: {job.burst}")
-
-	def setupGanttChart(self):
-		self.ganttFrame = Frame(self.window, bg=GREY1, highlightthickness=1, highlightbackground=GREY2, highlightcolor=GREY3)
-		self.ganttFrame.grid(row=0, column=1, rowspan=2, sticky="nsew")
-
-		self.fig, self.ax = plt.subplots(figsize=(10, 5))
-		self.canvas = FigureCanvasTkAgg(self.fig, master=self.ganttFrame)
-		self.canvas.get_tk_widget().pack(fill=BOTH, expand=True)
-
-	def updateGanttChart(self):
-		self.ax.clear()
-		self.ax.set_title('Gantt Chart')
-		self.ax.set_xlabel('Time ms')
-		self.ax.set_ylabel('Jobs')
-
-		job_colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
-		for i, (job, start) in enumerate(self.scheduler.chartInfo):
-			self.ax.broken_barh([(start, job.burst)], (i * 15, 9), facecolors=(job_colors[i % len(job_colors)]))
-			self.ax.text(start + job.burst / 2, i * 15 + 4.5, job.burst, ha='center', va='center', color='white')
-
-		self.ax.set_yticks([i * 15 + 4.5 for i in range(len(self.scheduler.chartInfo))])
-		self.ax.set_yticklabels([job.name for job, _ in self.scheduler.chartInfo])
-		self.canvas.draw()
-
-	def setupStatistics(self):
-		self.statsFrame = Frame(self.window, bg=GREY1, highlightthickness=1, highlightbackground=GREY2, highlightcolor=GREY3)
-		self.statsFrame.grid(row=2, column=1, sticky="nsew")
-
-		Label(self.statsFrame, text="Medium Wait Time:", bg=GREY1, fg=WHITE1, font=("Helvetica", 14)).grid(row=0, column=0, padx=10, pady=10, sticky="w")
-		self.mediumWaitTimeLabel = Label(self.statsFrame, text="0", bg=GREY1, fg=WHITE1, font=("Helvetica", 14))
-		self.mediumWaitTimeLabel.grid(row=0, column=1, padx=10, pady=10, sticky="e")
-
-		Label(self.statsFrame, text="Medium Turnaround Time:", bg=GREY1, fg=WHITE1, font=("Helvetica", 14)).grid(row=1, column=0, padx=10, pady=10, sticky="w")
-		self.mediumTurnaroundTimeLabel = Label(self.statsFrame, text="0", bg=GREY1, fg=WHITE1, font=("Helvetica", 14))
-		self.mediumTurnaroundTimeLabel.grid(row=1, column=1, padx=10, pady=10, sticky="e")
-
-	def updateStatistics(self, w, t):
-		self.mediumWaitTimeLabel.config(text=str(w))
-		self.mediumTurnaroundTimeLabel.config(text=str(t))
-
-	def setupResetButton(self):
-		self.resetButton = Button(self.jobInputFrame, text="Reset", command=self.scheduler.reset, bg=GREY3, fg=WHITE1)
-		self.resetButton.grid(row=7, column=0, columnspan=2, pady=10)
+		"""
+			OUTPUTS CONFIGURATION
+		"""
+		self.outputs = Frame(master=self.window, bg=GREY2, highlightthickness=.5, highlightbackground=GREY1, highlightcolor=GREY1, border=0)
+		self.outputs.grid(row=3, column=1, sticky="nsew")
 
 	"""
-		Functions
+		IMPORTANT METHODS
 	"""
-	def execute(self):
-		self.root.mainloop()
+	def update_list(self, jobs: list[Job]):
+		for j, header in enumerate(["Process:", "Arrival ms:", "Burst ms:", "Priority:"]):
+			label = Label(master=self.list, text=header, bg=GREY2, fg=WHITE1, font=("Arial", 10, "bold"), border=0, justify=LEFT)
+			label.grid(row=0, column=j, sticky="nsew", pady=10)
+
+	"""
+		OTHER METHODS
+	"""
+	def get_x_perc(self, x: int) -> int:
+		return int(x * .01 * self.root.winfo_screenwidth())
+	
+	def get_y_perc(self, y: int) -> int:
+		return int(y * .01 * self.root.winfo_screenheight())
+	
+	def start_move(self, event):
+		self.x = event.x
+		self.y = event.y
+
+	def stop_move(self, _):
+		self.x = None
+		self.y = None
+
+	def do_move(self, _):
+		x = self.root.winfo_pointerx() - self.x
+		y = self.root.winfo_pointery() - self.y
+		self.root.geometry(f"+{x}+{y}")
+
+	def toggle_window(self):
+		if self.root.state() == "normal":
+			self.root.state("zoomed")
+			self.window_button.config(text="🗗")
+		else:
+			self.root.state("normal")
+			self.window_button.config(text="🗖")
--- Scheduler/Scheduler.py
+++ Scheduler/Scheduler.py
@@ -1,112 +1,102 @@
-import time as Time
-from idlelib.debugobj_r import remote_object_tree_item
-
-from Job import Job, State
-from Gui import Gui
+from Job import Job, State, JobInfo
+from myGUI import GUI
 
 class Scheduler:
 	def __init__(self):
-		self.readyQueue: list[Job] = []
+		self.processes: list[Job] = []
 		self.jobs: list[Job] = []
 		self.queue: list[Job] = []
-		self.tick: int = 0
-		self.chartInfo = []
-		self.gui = Gui(self)
-		self.quantum: int = 4
+		self.chart_info: list[JobInfo] = []
+		self.quantum: int = 0
+		self.time: int = 0
+		self.gui: GUI = GUI(self)
 
-	def sleep(self, ms: int):
-		#Time.sleep(ms * .001)
-		self.tick = self.tick + ms
+	def run(self):
+		self.gui.root.mainloop()
 
-	def sleepUntil(self, ms: int):
-		self.sleep(ms - self.tick)
+	def sleep(self, time_ms: int):
+		self.time = self.time + time_ms
 
-	def addJob(self, job: Job):
-		self.readyQueue.append(job)
-		self.gui.updateJobsList()
+	def sleep_until(self, time: int):
+		self.sleep(time - self.time)
 
-	def removeJob(self, job: Job):
-		self.readyQueue.remove(job)
-		self.gui.updateJobsList()
+	def set_quantum(self, quantum: int):
+		if quantum < 1:
+			raise ValueError("Quantum must be greater than 0")
+		
+		self.quantum = quantum
 
-	def reset(self):
-		self.readyQueue.clear()
-		self.tick = 0
-		self.chartInfo.clear()
-		self.gui.updateJobsList()
-		self.gui.updateGanttChart()
-		self.gui.updateStatistics(0, 0)
+	def add_process(self, job: Job):
+		self.processes.append(job)
+		self.gui.update_list(self.processes)
 
-	# SJF
-	def sortSJF(self):
-		self.readyQueue.sort(key = lambda job: (job.burst, -job.priority))
-		self.gui.updateJobsList()
-	
-	def executeSJF(self):
-		self.tick = 0
-		self.chartInfo.clear()
-		self.sortSJF()
-		jobs = self.readyQueue.copy()
+	def rem_process(self, job: Job):
+		self.processes.remove(job)
+		self.gui.update_list(self.processes)
 
-		while jobs:
-			if jobs[0].arrival > self.tick:
-				self.sleepUntil(jobs[0].arrival)
-				
-			job: Job = jobs.pop(0)
-			start: int = self.tick
-			job.state = State.running
-			self.sleep(job.burst)
-			job.state = State.terminated
-			self.chartInfo.append((job, start))
-		self.gui.updateGanttChart()
-		self.updateTimes()
+	def clear(self):
+		self.jobs = []
+		self.chart_info = []
+		self.time = 0
 
-	# FCFS
-	def sortFCFS(self):
-		self.readyQueue.sort(key = lambda job: (job.arrival, -job.priority))
-		self.gui.updateJobsList()
-
-	def executeFCFS(self):
-		self.tick = 0
-		self.chartInfo.clear()
-		self.sortFCFS()
-		jobs = self.readyQueue.copy()
-
-		while jobs:
-			if jobs[0].arrival > self.tick:
-				self.sleepUntil(jobs[0].arrival)
-				
-			job: Job = jobs.pop(0)
-			start: int = self.tick
-			job.state = State.running
-			self.sleep(job.burst)
-			job.state = State.terminated
-			self.chartInfo.append((job, start))
-		self.gui.updateGanttChart()
-		self.updateTimes()
-
-	#RR
 	def queue_waiting_jobs(self):
-		while self.jobs and self.jobs[0].arrival <= self.tick:
+		while self.jobs and self.jobs[0].arrival <= self.time:
 			job = self.jobs.pop(0)
 			job.state = State.ready
 			self.queue.append(job)
 
-	def run(self):
-		if not self.readyQueue:
-			return
+	def FCFS(self):
+		self.jobs = self.processes.copy()
+		self.jobs.sort(key=lambda job: (job.arrival, job.priority))
 
-		self.jobs = self.readyQueue.copy()
+		while self.jobs:
+			if self.jobs[0].arrival > self.time:
+				self.sleep_until(self.jobs[0].arrival)
+
+			job: Job = self.jobs.pop(0)
+			execution_time: int = job.burst
+			job_info: JobInfo = JobInfo(job.name, self.time, execution_time)
+
+			job.state = State.running
+			job.burst = max(0, job.burst - execution_time)
+			self.sleep(execution_time)
+
+			job.state = State.terminated
+			self.chart_info.append(job_info)
+		self.clear()
+
+	def SJF(self):
+		self.jobs = self.processes.copy()
+		self.jobs.sort(key=lambda job: (job.burst, job.priority))
+
+		while self.jobs:
+			if self.jobs[0].arrival > self.time:
+				self.sleep_until(self.jobs[0].arrival)
+
+			job: Job = self.jobs.pop(0)
+			execution_time: int = job.burst
+			job_info: JobInfo = JobInfo(job.name, self.time, execution_time)
+
+			job.state = State.running
+			job.burst = max(0, job.burst - execution_time)
+			self.sleep(execution_time)
+
+			job.state = State.terminated
+			self.chart_info.append(job_info)
+		self.clear()
+
+	def RR(self):
+		self.jobs = self.processes.copy()
 		self.jobs.sort(key=lambda value: (value.arrival, value.priority))
 
 		while self.jobs or self.queue:
-			if not self.queue and self.jobs[0].arrival > self.tick:
-				self.sleepUntil(self.jobs[0].arrival)
+			if not self.queue and self.jobs[0].arrival > self.time:
+				self.sleep_until(self.jobs[0].arrival)
 
 			self.queue_waiting_jobs()
 			job = self.queue[0]
 			execution_time = min(self.quantum, job.burst)
-			self.chartInfo.append((job, self.tick))
+			job_info = JobInfo(job.name, self.time, execution_time)
 			job.state = State.running
 			job.burst = max(0, job.burst - execution_time)
 
@@ -121,25 +111,15 @@ class Scheduler:
 				job.state = State.terminated
 
 			self.queue.remove(job)
-		self.gui.updateGanttChart()
-		self.updateTimes()
+			self.chart_info.append(job_info)
+		self.clear()
 
-	def updateTimes(self):
-		total_turnaround = 0
-		total_waiting = 0
-		
-		for info in self.chartInfo:
-			waiting = info[1] - info[0].arrival
-			turnaround = info[0].burst + waiting
-			total_waiting += waiting
-			total_turnaround += turnaround
-		
-		num_jobs = len(self.readyQueue)
-		avg_turnaround = total_turnaround / num_jobs if num_jobs > 0 else 0
-		avg_waiting = total_waiting / num_jobs if num_jobs > 0 else 0
-		self.gui.updateStatistics(avg_waiting, avg_turnaround)
+scheduler = Scheduler()
 
-if __name__ == "__main__":
-	sch = Scheduler()
-	sch.gui.execute()
-	
+scheduler.add_process(Job("a", 10, 5, 0))
+scheduler.add_process(Job("b", 23, 23, 0))
+scheduler.add_process(Job("c", 43, 4, 0))
+scheduler.add_process(Job("d", 5, 3, 0))
+scheduler.add_process(Job("e", 43, 12, 0))
+
+scheduler.run()
